154cf7cf8ada4decc726b2c4bd327bb6d5db4e05
lupen92 <lupen@nezha.ovh>
2020-04-15 19:18:38 +0200

    update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,18 @@
 
 #!/usr/bin/env python
 
+#Importation des modules ci-dessous grace au fabric.api, installe avec la commande pip install fabric3
 from fabric.api import cd, run, env, task, prefix
 
+#Definition du hote sur lequel tourne l'env
 env.hosts = ['localhost']
 
+#Creation du fonction ping_client, permettant de faire un ping vers node-routeur
 @task
 def ping_client():
     run('ping node-routeur')
 
+#Creation du fonction run_playbook, permettant de demarrer le playbook vers node-routeur
 @task
 def run_playbook():
     with cd('/home/user-ansible/ansible2.7.10/local/bin'):
